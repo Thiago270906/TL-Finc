c7befa558a830c42601e9fd52a98049654d4bfce
Thiago Lopes <thiagoohmlopes@gmail.com>
2026-09-26 08:17:56 -0300

    feat: Desing para o aplicativo na hora de baixar

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "TL-Finc",
   description: "Controle financeiro pessoal",
+  manifest: "/manifest.json",
   icons: {
     icon: '/icon-tlfinc.png',
+    apple: '/icons/apple-touch-icon.png',
   }
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
